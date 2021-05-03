9a9faee2fd166b0ad8f72c41cdcab99d5c38283e
Shraey Chikker <shraey.chikker@bookmyshow.com>
2021-05-03 10:52:22 +0530

    ...

--- src/konva-canvas-renderer/index.js
+++ src/konva-canvas-renderer/index.js
@@ -622,8 +622,8 @@ const MainStage = (props) => {
       <button
         style={{
           position: "fixed",
-          top: 0,
-          left: 0,
+          bottom: 0,
+          right: 0,
           zIndex: 2,
         }}
         onClick={handleSeatRedraw}
